fe7bdb3d1fae9b2b8b9e07392f8b637e65aeb799
Nguyen Cong Tan <tancoder01@gmail.com>
2020-08-03 14:02:36 +0700

    Them file nghe python

---
+++ nghe.py
@@ -0,0 +1,25 @@
+# import speech_recognition
+
+# robot_ear = speech_recognition.Recognizer()
+# with speech_recognition.Microphone() as mic:
+# 	print("Robot: I'm Listening ... ")
+# 	audio = robot_ear.listen(mic)
+# try:
+# 	you = robot_ear.recognize_google(audio, language="fr-FR")
+# except:
+# 	you = "loi"
+
+# print(you)
+
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+	print("Speak anithing ...")
+	audio = r.listen(source)
+	try:
+		text = r.recognize_google(audio)
+		print("You said: {}".format(text))
+	except:
+		print("Sorry")
